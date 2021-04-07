ddb737c609537f73dea6c17f1f557fb37ff71d25
渡辺孝之 <na8esin.mpst@gmail.com>
2021-04-07 20:44:23 +0900

    csv: ちょっとやそっとじゃエラーにならない

--- bin/csv_sample.dart
+++ bin/csv_sample.dart
@@ -1,6 +1,8 @@
 import 'package:csv/csv.dart';
 
 void main(List<String> args) {
-  final res = const CsvToListConverter().convert('title,body\r\nタイトル,ボディ');
+  // 各行の列数が違ってもエラーが発生しない
+  final res = const CsvToListConverter().convert('title,body\r\nタイトル,ボディ,フッター');
   print(res);
+  print(const CsvToListConverter().convert("title,body\r\nタイトル,"));
 }
